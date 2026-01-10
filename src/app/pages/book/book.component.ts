a2c1675e4c93d2d9e83be81000f743b2d492480c
lior <liorgishry@microsoft.com>
2026-01-10 10:54:19 +0200

    more

--- src/app/pages/book/book.component.ts
+++ src/app/pages/book/book.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
-import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbCarouselModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { NoyaBookReviewsComponent } from "../../noya-book-reviews/noya-book-reviews.component";
-
-
 
 @Component({
   selector: 'noya-book',
   standalone: true,
-  imports: [NgbCarouselModule, NoyaBookReviewsComponent],
+  imports: [NgbCarouselModule, NgbNavModule, NoyaBookReviewsComponent],
   templateUrl: './book.component.html',
   styleUrl: './book.component.scss'
 })
 export class BookComponent {
-
+    active = 1;
 }
